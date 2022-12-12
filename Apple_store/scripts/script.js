555d7c97eab7d54859958958b193fd64f6247707
Mikael Krefors <krefors@hotmail.com>
2022-12-12 13:42:12 +0100

    Added movie slider

--- Apple_store/scripts/script.js
+++ Apple_store/scripts/script.js
@@ -14,10 +14,11 @@ document.querySelectorAll('.about .video-container .controls .control-btn').forE
   }
 })
 
+/*
 // OWL carousel autoplay for partners 
 let owl = $('.owl-carousel');
 owl.owlCarousel({
-    items:6, 
+    items:1, 
   
     loop:true,
     autoplay:true,
@@ -41,4 +42,25 @@ function carousel() {
   }
   x[myIndex - 1].style.display = 'block';
   setTimeout(carousel, 9000);
+}
+*/
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
